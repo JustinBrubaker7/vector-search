effcdb073f531ce4c00aea1696a36e054160bb11
Justin Brubaker <justinbrubaker7@gmail.com>
2024-03-07 20:19:21 -0700

    Update swagger.js with dynamic host and HTTPS support

--- swagger.js
+++ swagger.js
@@ -5,15 +5,14 @@ const doc = {
         title: 'Semantic Search - Bible Vector Search API',
         description: 'This is a simple API for searching the Bible using semantic search.',
     },
-    host: 'localhost:8080/search',
+    // Dynamically set the host based on the environment
+    host: process.env.NODE_ENV === 'production' ? 'vector.justinbrubaker.dev' : 'localhost:8080',
+    schemes: ['https'], // Ensure HTTPS is used, especially for production
 };
 
 const outputFile = './swagger-output.json';
 const routes = ['./routes/external.js'];
 
-/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
-root file where the route starts, such as index.js, app.js, routes.js, etc ... */
-
 swaggerAutogen(outputFile, routes, doc).then(() => {
-    require('./server.js');
+    require('./server.js'); // Make sure this is the entry point of your application
 });
